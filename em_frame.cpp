cd45acc875d88759932382b91848c4e45a43b953
kimhyunkang <kimhyunkang@gmail.com>
2013-01-23 10:40:15 +0900

    add WebView to the main frame

--- em_frame.cpp
+++ em_frame.cpp
@@ -1,7 +1,12 @@
 #include "em_frame.hpp"
+#include <wx/webview.h>
 
 EMFrame::EMFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
 {
     Centre();
+
+    viewer = wxWebView::New(this,
+                            wxID_ANY,
+                            "file:///Users/kimhyunkang/project/editmarked/temp.html");
 }
--- em_frame.hpp
+++ em_frame.hpp
@@ -1,7 +1,11 @@
 #include <wx/wx.h>
+#include <wx/webview.h>
 
 class EMFrame: public wxFrame
 {
 public:
     EMFrame(const wxString& title);
+
+private:
+    wxWebView *viewer;
 };
