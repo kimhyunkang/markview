4cd1c3162e936f027aead060c44c769e1b2ac629
kimhyunkang <kimhyunkang@gmail.com>
2013-01-23 10:40:25 +0900

    add Quit menu

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
 #include "main.hpp"
-#include "em_frame.hpp"
 
 IMPLEMENT_APP(EMApp)
 
+BEGIN_EVENT_TABLE(EMApp, wxApp)
+    EVT_MENU(wxID_EXIT, EMApp::OnQuit)
+END_EVENT_TABLE()
+
 bool EMApp::OnInit()
 {
-    EMFrame *frame = new EMFrame(wxT("EditMarked"));
+
+#ifdef __WXMAC__
+    // Mac users expect programs not to be terminated when main window closes
+    wxApp::SetExitOnFrameDelete(false);
+#endif
+
+    frame = new EMFrame("EditMarked");
+
+    wxMenuBar* menubar = new wxMenuBar();
+    wxMenu* filemenu = new wxMenu();
+
+    filemenu->Append(wxID_EXIT, "Quit\tCtrl-Q");
+    menubar->Append(filemenu, "File");
+
+#ifdef __WXMAC__
+    // set menu bar to be displayed when no frames are open
+    wxMenuBar::MacSetCommonMenuBar(menubar);
+#endif
+
+    frame->SetMenuBar(menubar);
     frame->Show(true);
 
     return true;
+}
+
+void EMApp::OnQuit(wxCommandEvent& evt)
+{
+    exit(0);
 }
--- main.hpp
+++ main.hpp
@@ -1,7 +1,15 @@
 #include <wx/wx.h>
+#include "em_frame.hpp"
 
 class EMApp: public wxApp
 {
+private:
+    EMFrame *frame;
+
 public:
+    DECLARE_EVENT_TABLE()
+
     virtual bool OnInit();
+
+    void OnQuit(wxCommandEvent& evt);
 };
